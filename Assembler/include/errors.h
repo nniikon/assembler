f0d2b36129c707cdd2b39c80597dd0d813e27ebc
nniikon <kolya.kasparov@gmail.com>
2023-10-19 17:57:47 +0300

    add code generation

---
+++ Assembler/include/errors.h
@@ -0,0 +1,4 @@
+DEF_ERR(NO_ERROR, "no error occurred")
+DEF_ERR(INVALID_CMD_NAME, "no such command was found")
+DEF_ERR(TOO_MANY_ARGS, "too many arguments for this command")
+DEF_ERR(INVALID_ARG, "unknown argument for this command")
