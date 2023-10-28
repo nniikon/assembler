e68fd95f2ef514b9598d4dedfa7426fbec4062bf
nniikon <kolya.kasparov@gmail.com>
2023-10-29 00:11:52 +0300

    add

--- SPU/source/SPU_graph.cpp
+++ SPU/source/SPU_graph.cpp
@@ -16,14 +16,20 @@ void renderRam_console(const int* ram, char* vramBuffer, const size_t size)
                 vramBuffer[(SPU_VRAM_WIDTH + 1) * y + x] = '\n';
                 continue;
             }
-
+            // TODO: add struct / enum.
             switch (vram[SPU_VRAM_WIDTH * y + x])
             {
             case 0:
                 vramBuffer[(SPU_VRAM_WIDTH + 1)* y + x] = '.';
                 break;
-            default:
+            case 100:
                 vramBuffer[(SPU_VRAM_WIDTH + 1)* y + x] = '*';
+                break;
+            case 200:
+                vramBuffer[(SPU_VRAM_WIDTH + 1)* y + x] = '%';
+                break;
+            default:
+                vramBuffer[(SPU_VRAM_WIDTH + 1)* y + x] = '&';
                 break;
             }
         }
